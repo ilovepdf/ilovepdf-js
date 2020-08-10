c1aa7e85dc40c3949bacf28ad976e61e5e2b35db
Diego Lao Tebar <diego.lao@ilovepdf.com>
2020-08-10 14:39:47 +0200

    Remove unused function

--- src/ILovePDFApi.test.ts
+++ src/ILovePDFApi.test.ts
@@ -201,16 +201,6 @@ function createFileToAdd(): Promise<ILovePDFFile> {
     })
 }
 
-function arrayBufferToBase64(buffer: ArrayBuffer) {
-    var binary = '';
-    var bytes = new Uint8Array(buffer);
-    var len = bytes.byteLength;
-    for (var i = 0; i < len; i++) {
-        binary += String.fromCharCode( bytes[ i ] );
-    }
-    return btoa( binary );
-}
-
 function arrayBufferToString(buffer: ArrayBuffer): string {
     let str = '';
 
