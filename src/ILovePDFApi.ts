2038b737daa12bbb6854c47222bb830d28ab5b04
Diego Lao Tebar <diego.lao@ilovepdf.com>
2020-08-31 12:33:20 +0200

    Added new method to get a signature

--- src/ILovePDFApi.ts
+++ src/ILovePDFApi.ts
@@ -1,14 +1,12 @@
 import Auth from "@ilovepdf/ilovepdf-core/dist/auth/Auth";
 import JWT from "@ilovepdf/ilovepdf-core/dist/auth/JWT";
-import ILovePDFCoreApi, { UpdateSignerData } from '@ilovepdf/ilovepdf-core/dist/ILovePDFCoreApi';
-import { TaskParams } from "@ilovepdf/ilovepdf-core/dist/tasks/Task";
 import TaskFactory, { TaskFactoryI } from "@ilovepdf/ilovepdf-core/dist/tasks/TaskFactory";
+import ILovePDFCoreApi from '@ilovepdf/ilovepdf-core/dist/ILovePDFCoreApi';
 import TaskI from "@ilovepdf/ilovepdf-core/dist/tasks/TaskI";
 import ILovePDFTool from "@ilovepdf/ilovepdf-core/dist/types/ILovePDFTool";
-import GetSignerResponse from "@ilovepdf/ilovepdf-core/dist/types/responses/GetSignerResponse";
 import XHRInterface from "@ilovepdf/ilovepdf-core/dist/utils/XHRInterface";
 import XHRPromise from "./XHRPromise";
-import GetSignatureTemplateResponse from "@ilovepdf/ilovepdf-core/dist/types/responses/GetSignatureTemplateResponse";
+import SignTask, { TemplateElement } from "@ilovepdf/ilovepdf-core/dist/tasks/sign/SignTask";
 
 export interface ILovePDFApiI {
     /**
@@ -17,28 +15,15 @@ export interface ILovePDFApiI {
      */
     newTask: (taskType: ILovePDFTool) => TaskI;
     /**
-     * Updates a signer that was also the requester in a signature process.
-     * @param signerToken - Signer token of the signer that has to be updated.
-     * @param data - Object with values to change.
+     * Retrieves a signature template.
+     * @param templateTaskId - Task id of the task that created the template.
      */
-    updateSigner: (signerToken: string, data: UpdateSignerData) => Promise<GetSignerResponse>;
+    getSignatureTemplate: (templateTaskId: string) => Promise<TemplateElement>;
     /**
-     * Updates a signer email in a signature process.
-     * @param requesterToken - Request token of the signer that has to be updated.
-     * @param email - New email.
+     * Retrieves a signature task.
+     * @param signatureTaskId - Signature task id.
      */
-    updateSignerEmail: (requesterToken: string, email: string) => Promise<GetSignerResponse>;
-    /**
-     * Updates a signer phone in a signature process.
-     * @param requesterToken - Request token of the signer that has to be updated.
-     * @param phone - New phone.
-     */
-    updateSignerPhone: (requesterToken: string, phone: string) => Promise<GetSignerResponse>;
-    /**
-     * Gets a template previously created.
-     * @param taskId - Task id of the task that created the template.
-     */
-    getSignatureTemplate: (taskId: string) => Promise<GetSignatureTemplateResponse>;
+    getSignature: (signatureTaskId: string) => Promise<SignTask>;
 }
 
 export type ILovePDFApiParams = {
@@ -56,24 +41,25 @@ export default class ILovePDFApi implements ILovePDFApiI {
         this.taskFactory = new TaskFactory();
     }
 
+    /**
+     * @inheritdoc
+     */
     public newTask(taskType: ILovePDFTool): TaskI {
         return this.taskFactory.newTask(taskType, this.auth, this.xhr);
     }
 
-    public async updateSigner(signerToken: string, data: UpdateSignerData) {
-        return ILovePDFCoreApi.updateSigner(this.auth, this.xhr, signerToken, data);
-    }
-
-    public async updateSignerEmail(requesterToken: string, email: string) {
-        return ILovePDFCoreApi.updateSignerEmail(this.auth, this.xhr, requesterToken, email);
-    }
-
-    public async updateSignerPhone(requesterToken: string, phone: string) {
-        return ILovePDFCoreApi.updateSignerPhone(this.auth, this.xhr, requesterToken, phone);
-    }
-
+    /**
+     * @inheritdoc
+     */
     public async getSignatureTemplate(taskId: string) {
         return ILovePDFCoreApi.getSignatureTemplate(this.auth, this.xhr, taskId);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public async getSignature(signatureTaskId: string): Promise<SignTask> {
+        return ILovePDFCoreApi.getSignature(this.auth, this.xhr, signatureTaskId);
     }
 
 }
